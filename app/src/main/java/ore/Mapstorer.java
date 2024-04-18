34d6a13f1135605f34339c33d304d880bfb10959
YIYANZ2 <yiyanz2@student.unimelb.edu.au>
2024-04-19 01:24:00 +1000

    add map storer

---
+++ app/src/main/java/ore/Mapstorer.java
@@ -0,0 +1,32 @@
+package ore;
+
+public class Mapstorer {
+    protected final static String map_0 =
+            "    xxxxx          " + // 0 (19)
+                    "    x...x          " + // 1
+                    "    x*..x          " + // 2
+                    "  xxx...xx         " + // 3
+                    "  x......x         " + // 4
+                    "xxx...RD.x   xxxxxx" + // 5
+                    "x.....RD.xxxxx....x" + // 6
+                    "x...*............ox" + // 7
+                    "xxxxx.DDD.xPxx...ox" + // 8
+                    "    x.....xxxxxxxxx" + // 9
+                    "    xxxxxxx        ";  //10
+    protected final static int nbHorzCells_0 = 19;
+    protected final static int nbVertCells_0 = 11;
+
+    protected final static String map_1 =
+            "xxxxxxxxxxxx" + // 0  (14)
+                    "x..........x" + // 0  (14)
+                    "x....RB....x" + // 1
+                    "xo...R.*...x" + // 2
+                    "xo...RDDDDDx" + // 3
+                    "xP....ERRRRx" + // 4
+                    "x....RRR*.xx" + // 5
+                    "x..........x" + // 6
+                    "xxxxxxxxxxxx";  // 7
+
+    protected final static int nbHorzCells_1 = 12;
+    protected final static int nbVertCells_1 = 9;
+}
