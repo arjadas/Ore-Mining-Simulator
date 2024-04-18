ee06b5d8d74bf9307a76ba0b58959c261d52ae33
Min <minthuh@student.unimelb.edu.au>
2024-04-18 20:52:04 +1000

    Deleted MapManger, and included common functions into Machine

--- app/src/main/java/ore/Machine.java
+++ app/src/main/java/ore/Machine.java
@@ -1,4 +1,16 @@
 package ore;
 
+import ch.aplu.jgamegrid.Actor;
+import ch.aplu.jgamegrid.Location;
+
+import java.awt.*;
+import java.util.List;
+
 public interface Machine {
+
+    public void autoMoveNext(boolean isFinished);
+
+    public boolean canMove(Location location);
+
+
 }
